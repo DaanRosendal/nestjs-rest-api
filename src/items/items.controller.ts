1e39730e2e7c3da78906fa54a3d8d7c2a9b9b06f
Daan <daanrosendal@outlook.com>
2021-12-24 21:25:33 +0100

    create endpoints for items

---
+++ src/items/items.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
+
+@Controller('items')
+export class ItemsController {
+  @Get()
+  findAll(): string {
+    return 'Get all items';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id): string {
+    return `Item ${id}`;
+  }
+
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): string {
+    return `Name: ${createItemDto.name} Description: ${createItemDto.description}`;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): string {
+    return `Delete ${id}`;
+  }
+
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
+    return `Update ${id} - Item: ${updateItemDto.name}`;
+  }
+}
